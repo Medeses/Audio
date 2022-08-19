e1e708bebaa46e97287c0f67957ab0594dc6c780
Marius Bäumle <marius.baeumle@gmail.com>
2022-08-19 22:55:53 +0200

    Created new branch for shelf filters

--- SimpleEQ/Source/PluginProcessor.h
+++ SimpleEQ/Source/PluginProcessor.h
@@ -145,3 +145,6 @@ private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
 };
+
+
+// TODO: Add Low and High Shelves
